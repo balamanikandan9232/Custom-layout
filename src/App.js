fd46fe42b014499c5f505ec8f8659ccebfb0f18d
Balamanikandan <balamanikandan9232@gmail.com>
2024-08-16 14:19:42 +0530

    All my files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./App.css";
 
-function App() {
+const Index = () => {
+  const [users, setUser] = useState([]);
+
+  const getuser = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=9"
+    );
+    const finaldata = await res.json();
+    console.log(finaldata)
+    setUser(finaldata);
+  };
+  useEffect(() => {
+    getuser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 className="Header">
+        A collective list of free APIs for use in software and web development
+      </h3>
+      <div className="search-bar">
+        <button className="icon">
+          <SearchIcon />
+        </button>
+        <input type="search" placeholder="Find development,games,images APIs" />
+      </div>
+      <div className="card-header">
+        <h3 className="card-h">Featured APIs of this week </h3>
+      </div>
+      <div className="card-container">
+        <div className="card">
+          {users.map((user,id) => (
+            <div className="card-inner" key={id}>
+              <img src={user.thumbnailUrl} width={100} />
+              <span className="card-title" >{user.title}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Index;
